59bf37dcb6d375af527e3c2995cc3aaa7b1a57ff
Sudar <sudar@sudarmuthu.com>
2011-02-03 12:36:08 +0530

    Updated code to handle HTC Music Player

--- android/src/com/sudarmuthu/android/adjustvolume/AdjustVolume.java
+++ android/src/com/sudarmuthu/android/adjustvolume/AdjustVolume.java
@@ -1,7 +1,6 @@
 package com.sudarmuthu.android.adjustvolume;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -9,23 +8,16 @@ import android.content.ServiceConnection;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import at.abraxas.amarino.AmarinoIntent;
-
-import com.android.music.IMediaPlaybackService;
 
 public class AdjustVolume extends Activity {
-	// change this to your Bluetooth device address 
-//	private static final String DEVICE_ADDRESS =  "00:06:66:03:73:7B"; //"00:06:66:03:73:7B";
 	
     protected static final String TAG = "AdjustVolume";
+	private boolean isHtc;
 
-//	private ArduinoReceiver arduinoReceiver = new ArduinoReceiver();
-    
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,10 +54,20 @@ public class AdjustVolume extends Activity {
 //		registerReceiver(arduinoReceiver, new IntentFilter(AmarinoIntent.ACTION_RECEIVED));
 
 		Intent i = new Intent();
-		i.setClassName("com.android.music", "com.android.music.MediaPlaybackService");
-		 
 		ServiceConnection conn = new MediaPlayerServiceConnection();
-		this.bindService(i, conn, Context.BIND_AUTO_CREATE);
+		
+		isHtc = true;
+		i.setClassName("com.htc.music", "com.htc.music.MediaPlaybackService");
+		
+        if (!this.bindService(i, conn, Context.MODE_PRIVATE)) {
+        	isHtc = false;
+            i.setClassName("com.android.music", "com.android.music.MediaPlaybackService");
+            this.bindService(i, conn, Context.MODE_PRIVATE);
+        }
+		
+//		i.setClassName("com.android.music", "com.android.music.MediaPlaybackService");
+//		 
+//		this.bindService(i, conn, Context.MODE_PRIVATE);
         
 		Log.d(TAG, "Binded Service");
 				
@@ -85,101 +87,43 @@ public class AdjustVolume extends Activity {
 //		unregisterReceiver(arduinoReceiver);
 	}
 	
-
-	/**
-	 * ArduinoReceiver is responsible for catching broadcasted Amarino
-	 * events.
-	 * 
-	 * It extracts data from the intent and updates the graph accordingly.
-	 */
-	public class ArduinoReceiver extends BroadcastReceiver {
-
-		@Override
-		public void onReceive(Context context, Intent intent) {
-	        final AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-
-			String data = null;
-			
-//			 the device address from which the data was sent, we don't need it here but to demonstrate how you retrieve it
-//			final String address = intent.getStringExtra(AmarinoIntent.EXTRA_DEVICE_ADDRESS);
-			
-			// the type of data which is added to the intent
-			final int dataType = intent.getIntExtra(AmarinoIntent.EXTRA_DATA_TYPE, -1);
-			
-			// we only expect String data though, but it is better to check if really string was sent
-			// later Amarino will support differnt data types, so far data comes always as string and
-			// you have to parse the data to the type you have sent from Arduino, like it is shown below
-			if (dataType == AmarinoIntent.STRING_EXTRA){
-				data = intent.getStringExtra(AmarinoIntent.EXTRA_DATA);
-				
-				if (data != null){
-					Log.d(TAG, "Received Data: " + data);
-					if (data.equals("Plus")) {
-						audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);						
-					}
-					
-					if (data.equals("Minus")) {
-						audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);						
-					}
-				}
-			}
-		}
-	}
-	
-//	public class MediaPlayerServiceConnection implements ServiceConnection {
-		
-//		public IMediaPlaybackService service;		
-//        public void onServiceConnected(ComponentName name, IBinder boundService) {
-//            Log.d(TAG, "Connected! Name: " + name.getClassName());
-// 
-//            // This is the important line
-//            service = IMediaPlaybackService.Stub.asInterface((IBinder) boundService);
-// 
-//            // If all went well, now we can use the interface
-// 
-//            try {
-//					Log.d(TAG, "Playing track: " + service.getTrackName());
-//                //Tell the player to pause the song
-//                    service.pause();
-// 
-//                //Log.i("MediaPlayerServiceConnection", "By artist: " + service.getArtistName());
-//                if (service.isPlaying()) {
-//                    Log.d(TAG, "Music player is playing.");
-//                } else {
-//                    Log.d(TAG, "Music player is not playing.");
-//                }
-//            } catch (RemoteException e) {
-//            	Log.d(TAG, "Some exception");
-//            	e.printStackTrace();
-//            }
-//        }
-// 
-//        public void onServiceDisconnected(ComponentName name) {
-//            service=null;
-//            Log.d(TAG, "Disconnected!");
-//        }
-//    }
-	
-	
 	private class MediaPlayerServiceConnection implements ServiceConnection {
-
-		public IMediaPlaybackService mService;
+    	public com.htc.music.IMediaPlaybackService mServiceHtc;
+    	public com.android.music.IMediaPlaybackService mServiceAndroid;
 
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder service) {
 			Log.i("MediaPlayerServiceConnection", "Connected! Name: " + name.getClassName());
 
 			// This is the important line
-			mService = IMediaPlaybackService.Stub.asInterface(service);
-
+    		if (isHtc)
+    			mServiceHtc = com.htc.music.IMediaPlaybackService.Stub.asInterface(service);
+			else
+				mServiceAndroid = com.android.music.IMediaPlaybackService.Stub.asInterface(service);
+			
 			// If all went well, now we can use the interface
 			try {
-				Log.i("MediaPlayerServiceConnection", "Playing track: " + mService.getTrackName());
-				Log.i("MediaPlayerServiceConnection", "By artist: " + mService.getArtistName());
-				if (mService.isPlaying()) {
-					Log.i("MediaPlayerServiceConnection", "Music player is playing.");
+				
+				if (isHtc) {
+					
+					Log.i("MediaPlayerServiceConnection", "Playing track: " + mServiceHtc.getTrackName());
+					Log.i("MediaPlayerServiceConnection", "By artist: " + mServiceHtc.getArtistName());
+					if (mServiceHtc.isPlaying()) {
+						Log.i("MediaPlayerServiceConnection", "Music player is playing.");
+						// Next Track
+						mServiceHtc.next();
+					} else {
+						Log.i("MediaPlayerServiceConnection", "Music player is not playing.");
+					}
 				} else {
-					Log.i("MediaPlayerServiceConnection", "Music player is not playing.");
+					
+					Log.i("MediaPlayerServiceConnection", "Playing track: " + mServiceAndroid.getTrackName());
+					Log.i("MediaPlayerServiceConnection", "By artist: " + mServiceAndroid.getArtistName());
+					if (mServiceAndroid.isPlaying()) {
+						Log.i("MediaPlayerServiceConnection", "Music player is playing.");
+					} else {
+						Log.i("MediaPlayerServiceConnection", "Music player is not playing.");
+					}
 				}
 			} catch (Exception e) {
 				Log.i("MediaPlayerServiceConnection", "Some Exception");
